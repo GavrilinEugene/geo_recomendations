d8bc75abf61f98af7c3cc7c60662bc4939aa4659
GavrilinEugene <julentver1@gmail.com>
2021-10-12 21:09:54 +0300

    add script for colelcting izochrones

--- src/utils.py
+++ src/utils.py
@@ -10,3 +10,31 @@ def load_pickle(file_path):
 def save_pickle(obj, filepath): 
     with open(filepath, 'wb') as handle:
         pickle.dump(obj, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+def flatten_json(nested_json, exclude=['']):
+    """Flatten json object with nested keys into a single level.
+        Args:
+            nested_json: A nested json object.
+            exclude: Keys to exclude from output.
+        Returns:
+            The flattened json object if successful, None otherwise.
+    """
+    out = {}
+
+    def flatten(x, name='', exclude=exclude):
+        if type(x) is dict:
+            for a in x:
+                if a not in exclude: flatten(x[a], name + a + '_')
+        elif type(x) is list:
+            i = 0
+            for a in x:
+                flatten(a, name + str(i) + '_')
+                i += 1
+        else:
+            out[name[:-1]] = x
+
+    flatten(nested_json)
+    return out
+
+       
