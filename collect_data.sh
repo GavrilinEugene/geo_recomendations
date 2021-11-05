04ebef8daa56acdc54e341ffab9c2b041d964929
GavrilinEugene <julentver1@gmail.com>
2021-11-05 11:41:37 +0300

    collect_data run script

---
+++ collect_data.sh
@@ -0,0 +1,9 @@
+source venv/bin/activate
+echo 'текущий интерпретатор:'
+which python3
+echo 'выгружаем изохроны:'
+python3 src/collect_isochrones.py
+echo 'выгружаем данные по инфраструктуре:'
+python3 src/collect_data.py -l $1 -p $2 -s $3 -port $4
+echo 'создаём таблицы с административным делением в БД'
+python3 src/create_adm_tables.py -l $1 -p $2 -s $3 -port $4
